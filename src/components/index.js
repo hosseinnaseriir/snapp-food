a81eab646c7362d1fa3b2a180149b538b6ecb21f
Hossein Naseri <hossein@ethicadata.com>
2022-07-31 16:40:58 +0430

    Create Project Skeleton

---
+++ src/components/index.js
@@ -0,0 +1,3 @@
+import VendorCard from "./shared/vendorCard";
+
+export { VendorCard };
---
+++ src/screens/Home/index.js
@@ -0,0 +1,8 @@
+import React from "react";
+import { VendorCard } from "./../../components";
+
+function Index() {
+  return <VendorCard />;
+}
+
+export default Index;
